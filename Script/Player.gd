efe411fb58adaaabf5e8c1cc8df0cdf6e00de6ab
sirpineapplehead <107498118+sirpineapplehead@users.noreply.github.com>
2024-01-25 15:25:45 -0800

    added a few things

--- Script/Player.gd
+++ Script/Player.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 var speed = 400
 
+var inventory: Dictionary = {}
+
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
@@ -9,3 +11,15 @@ func get_input():
 func _physics_process(_delta):
 	get_input()
 	move_and_slide()
+
+	for area in $ReachArea.get_overlapping_areas():
+		if area.is_in_group('Item') and area.mouse_in_area == true:
+			if Input.is_action_pressed("Pickup"):
+				inventory[str(area)] = area
+				area.visible = false
+
+
+
+
+
+
---
+++ Script/item.gd
@@ -0,0 +1,9 @@
+extends Sprite2D
+
+var items: Dictionary = {}
+
+func _ready():
+	pass
+
+func _process(delta):
+	pass
