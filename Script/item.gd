d7bee949db92cd89edf728530bd5e37f7fdfe964
sirpineapplehead <107498118+sirpineapplehead@users.noreply.github.com>
2024-01-27 10:12:35 -0800

    added some stuff

--- Script/item.gd
+++ Script/item.gd
@@ -1,9 +1,23 @@
 extends Sprite2D
 
-var items: Dictionary = {}
+@onready var items: Dictionary = {
+	'plank of wood': preload("res://Assets/Sprites/plank of wood.png")
+	
+	
+	
+	
+	
+	}
+
+var is_item
 
 func _ready():
-	pass
+	var random_items = []
+	for i in items.keys():
+		random_items.append(i)
+	is_item = items[random_items[randi_range(0,random_items.size() - 1)]]
+	print(is_item)
+	texture = is_item
 
-func _process(delta):
+func _process(_delta):
 	pass
