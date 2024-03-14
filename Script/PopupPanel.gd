fd1b453cd63fb4e8a484137f76770fac05075fd6
sirpineapplehead <107498118+sirpineapplehead@users.noreply.github.com>
2024-03-13 19:20:49 -0700

    just connected some buttons

--- Script/PopupPanel.gd
+++ Script/PopupPanel.gd
@@ -2,6 +2,8 @@ extends Panel
 
 var last_position
 
+var currently_over
+
 func _process(_delta):
 	if last_position != null:
 		global_position = last_position
@@ -10,6 +12,13 @@ func open(object_selected):
 	self.visible = true
 	last_position = global_position
 	if object_selected == 'GolfCart':
+		currently_over = object_selected
 		$Button.text = 'Travel to town'
 		$Button2.text = 'Check storage'
-	
+
+func _on_button_button_up():
+	if currently_over:
+		pass
+
+func _on_button_2_button_up():
+	pass
