57cee1e1c619578603531096234b805182bc1c8e
sirpineapplehead <107498118+sirpineapplehead@users.noreply.github.com>
2024-04-26 18:23:39 -0700

    fixed some of the crafting system

--- Script/craftable_item.gd
+++ Script/craftable_item.gd
@@ -2,6 +2,10 @@ extends Control
 
 @export var item: String
 
+var has_enough_resources = false
+
+var resources_passed = 0
+
 func _ready():
 	$TextureButton.texture_normal = Global.item_sprites[item]
 
@@ -12,19 +16,20 @@ func craft_item():
 	for resource_needed in Global.dictionary_of_items[item]['Cost'].keys():
 		if Global.Player.inventory.has(resource_needed):
 			if Global.Player.inventory[resource_needed] >= Global.dictionary_of_items[item]['Cost'][resource_needed]:
+				resources_passed += 1
 				if Global.Player.inventory[resource_needed] == Global.dictionary_of_items[item]['Cost'][resource_needed]:
 					for child in get_node('/root/BaseGame/Player/MiniMenu/TabContainer/Inventory/ScrollContainer/ItemGridContainer').get_children():
 						if child.is_item in Global.dictionary_of_items[item]['Cost']:
 							Global.Player.inventory[resource_needed] -= Global.dictionary_of_items[item]['Cost'][resource_needed]
 							Global.Player.current_amount_of_items -= Global.dictionary_of_items[item]['Cost'][resource_needed]
 							child.queue_free()
+							print(resource_needed)
 							break
 					
 					Global.Player.inventory.erase(resource_needed)
 				
 				elif Global.Player.inventory[resource_needed] > Global.dictionary_of_items[item]['Cost'][resource_needed]:
 					for child in get_node('/root/BaseGame/Player/MiniMenu/TabContainer/Inventory/ScrollContainer/ItemGridContainer').get_children():
-						
 						if child.is_item in Global.dictionary_of_items[item]['Cost']:
 							Global.Player.inventory[resource_needed] -= Global.dictionary_of_items[item]['Cost'][resource_needed]
 							Global.Player.current_amount_of_items -= Global.dictionary_of_items[item]['Cost'][resource_needed]
@@ -32,5 +37,16 @@ func craft_item():
 							break
 					
 					Global.Player.inventory[resource_needed] -= Global.dictionary_of_items[item]['Cost'][resource_needed]
-				
-				get_node('/root/BaseGame').add_item_to_inventory(item)
+			else:
+				break
+		else:
+			break
+	
+	if resources_passed == Global.dictionary_of_items[item]['Cost'].size():
+		has_enough_resources = true
+	
+	if has_enough_resources == true:
+		get_node('/root/BaseGame').add_item_to_inventory(item)
+	
+	resources_passed = 0
+	has_enough_resources = false
